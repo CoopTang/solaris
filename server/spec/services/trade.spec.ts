40e914ef79e329c9d8c6b0dda87f83ad72c50c9d
Michael Cooper <macoop2363@gmail.com>
2024-01-23 12:09:16 -0700

    Add tests to ensure you can't trade in no diplomacy games and to non-allied players

--- server/spec/services/trade.spec.ts
+++ server/spec/services/trade.spec.ts
@@ -1,3 +1,5 @@
+import DiplomacyService from '../../services/diplomacy';
+import { DiplomaticState } from '../../services/types/Diplomacy';
 import TradeService from "../../services/trade"
 import ValidationError from "../../errors/validation";
 
@@ -5,7 +7,8 @@ const gameRepo: any = {}
 const eventRepo: any = {}
 const userService: any = {}
 const playerService: any = {}
-const diplomacyService: any = {}
+const diplomacyUpkeepService: any = {};
+const diplomacyService: DiplomacyService = new DiplomacyService(gameRepo, eventRepo, diplomacyUpkeepService);
 const ledgerService: any = {}
 const achievementService: any = {}
 const reputationService: any = {}
@@ -19,9 +22,6 @@ const _playerAliasA: string = 'Player 1';
 
 const _playerIdB: any = 2;
 const _playerAliasB: string = 'Player 2';
-
-const _playerIdC: any = 3;
-const _playerAliasC: string = 'Player 3';
 
 describe("TradeService", () => {
     
@@ -45,79 +45,75 @@ describe("TradeService", () => {
     })
 
     describe("sendStar()", () => {
-        let game: any = {
-            constants: {
-                star: {
-                    resources: {
-                        minNaturalResources: 10,
-                        maxNaturalResources: 50
+        const setupPlayerDiplomacyGame = (playerAStatusToB: DiplomaticState, playerBStatusToA: DiplomaticState) => {
+            const game: any = {
+                constants: {
+                    star: {
+                        resources: {
+                            minNaturalResources: 10,
+                            maxNaturalResources: 50
+                        }
                     }
-                }
-            },
-            galaxy: {
-                players: [
-                    {
-                        _id: _playerIdA,
-                        alias: _playerAliasA,
-                        diplomacy: [
-                            {
-                                playerId: _playerIdB,
-                                status: "enemies"
-                            },
-                            {
-                                playerId: _playerIdC,
-                                status: "allies"
-                            }
-                        ]
-                    },
-                    {
-                        _id: _playerIdB,
-                        alias: _playerAliasB,
-                        diplomacy: [
-                            {
-                                playerId: _playerIdA,
-                                status: "allies"
-                            },
-                            {
-                                playerId: _playerIdC,
-                                status: "allies"
-                            }
-                        ]
-                    },
-                    {
-                        _id: _playerIdC,
-                        alias: _playerAliasC,
-                        diplomacy: [
-                            {
-                                playerId: _playerIdA,
-                                status: "enemies"
-                            },
-                            {
-                                playerId: _playerIdB,
-                                status: "allies"
-                            }
-                        ]
+                },
+                galaxy: {
+                    players: [
+                        {
+                            _id: _playerIdA,
+                            alias: _playerAliasA,
+                            diplomacy: [
+                                {
+                                    playerId: _playerIdB,
+                                    status: playerAStatusToB
+                                }
+                            ]
+                        },
+                        {
+                            _id: _playerIdB,
+                            alias: _playerAliasB,
+                            diplomacy: [
+                                {
+                                    playerId: _playerIdA,
+                                    status: playerBStatusToA
+                                }
+                            ]
+                        }
+                    ],
+                    stars: [
+                        {
+                            _id: 1,
+                            ownedByPlayerId: _playerIdA
+                        }
+                    ]
+                },
+                settings: {
+                    diplomacy: {
+                        enabled: 'enabled'
                     }
-                ]
-            },
-            settings: {
-                diplomacy: {
-                    enabled: 'enabled'
                 }
             }
-        }
+    
+            return game;
+        };
+
+        it("should transfer onwership of star between two allied players", async () => {
+            const game = setupPlayerDiplomacyGame("allies", "allies")
+        })
 
         it("should not allow gifts when aliiances are disabled", async () => {
+            const game = setupPlayerDiplomacyGame("neutral", "neutral")
             game.settings.diplomacy.enabled = 'disabled'
     
-            await expectAsync(tradeService.sendStar(game, _playerIdA, {}, _playerIdB))
+            await expectAsync(tradeService.sendStar(game, _playerIdA, _playerIdB, {}))
                 .toBeRejectedWithError(ValidationError, 'Cannot Gift Stars in Non-Alliance Games');
         })
         
         it("should not gift star to unallied player", async () => {
-            game.settings.diplomacy.enabled = 'enabled'
-    
-            await expectAsync(tradeService.sendStar(game, _playerIdA, {}, _playerIdB))
+            const game1 = setupPlayerDiplomacyGame("neutral", "neutral")
+            const game2 = setupPlayerDiplomacyGame("allies", "enemies")
+
+            await expectAsync(tradeService.sendStar(game1, _playerIdA, _playerIdB, {}))
+                .toBeRejectedWithError(ValidationError, "Cannot Gift Stars to Non-Allied Players");
+            await expectAsync(tradeService.sendStar(game1, _playerIdA, _playerIdB, {}))
                 .toBeRejectedWithError(ValidationError, "Cannot Gift Stars to Non-Allied Players");
         })
     })
